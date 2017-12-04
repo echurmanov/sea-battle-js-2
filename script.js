ededfd806d09d9beb090a839bd977181928fb496
Eugene Churmanov <echurmanov@yandex-team.ru>
2017-12-04 19:13:54 +0500

    Сделана генерация кораблей

--- script.js
+++ script.js
@@ -30,6 +30,23 @@ function Game(field1, field2){
   }
 
 
+  this.getNearCell = function(fieldData, x, y) {
+    const res = [];
+    for(let dx=-1;dx<=1;dx++){
+      for(let dy=-1;dy<=1;dy++){
+        if(x+dx>=0 && y+dy>=0 && x+dx<this.size && y+dy<this.size
+          && !(dx == 0 && dy == 0)
+        ) {
+          res.push(fieldData[x+dx][y+dy]);
+        }
+      }
+    }
+
+
+    return res;
+  };
+
+
   this.renderField = function(field, data) {
     const table=document.createElement('table');
     field.appendChild(table);
@@ -66,6 +83,73 @@ function Game(field1, field2){
 
     this.renderField(field2, data2);
   };
+
+
+  this.checkPlaceShip = function (data, cells) {
+    let canPlace = true;
+    for (let i = 0; i < cells.length; i++) {
+      canPlace = canPlace && this.getNearCell(data, cells[i].x, cells[i].y).indexOf(2) === -1;
+    }
+
+    return canPlace;
+  };
+
+
+  this.placeRandomShip = function(data, size) {
+    let canPlace = true;
+    let dir, x, y;
+
+    do {
+      canPlace = true;
+      dir = (Math.random() > 0.5) ? 'H' : 'V';
+
+      x = Math.floor(Math.random() * this.size);
+      y = Math.floor(Math.random() * this.size);
+
+      if (dir === 'H') {
+        if (x > this.size - size) {
+          canPlace = false;
+        }
+      } else {
+        if (y > this.size - size) {
+          canPlace = false;
+        }
+      }
+
+      if (canPlace) {
+        const cells = [];
+        for (let s = 0; s < size; s++) {
+          let dx = x, dy = y;
+          if(dir==='H'){
+            dx+=s;
+          }else{
+            dy+=s;
+          }
+
+          cells.push({x: dx, y: dy});
+        }
+        canPlace = this.checkPlaceShip(data, cells);
+      }
+    } while(!canPlace);
+
+    for (let s = 0; s < size; s++) {
+      let dx = x, dy = y;
+      if(dir==='H'){
+        dx+=s;
+      }else{
+        dy+=s;
+      }
+
+      data[dx][dy] = SHIP;
+    }
+
+  };
+
+  for (let i = 0; i < shipConfig.length; i++) {
+    this.placeRandomShip(data1, shipConfig[i]);
+    this.placeRandomShip(data2, shipConfig[i]);
+  }
+
 }
 
 
@@ -77,6 +161,8 @@ function init() {
     document.getElementById('field-1'),
     document.getElementById('field-2')
   );
+
+
 
   game.render();
 
